ac94e5785f629e289dc307b67a76db2dcd3b58f6
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2023-07-13 22:54:15 +0300

    Delete projects/user-registeration directory

--- projects/user-registeration/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of user-registeration
- */
-
-export * from './lib/user-registeration.service';
-export * from './lib/user-registeration.component';
-export * from './lib/user-registeration.module';
